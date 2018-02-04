b777f90e4de314f8a74fdebb19e4d5b070ea6ae5
fmbf <circumnavigate.the.globe@gmail.com>
2018-02-04 10:52:07 -0800

    Create basic seeds for artists, fix index for Artists Controller

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,11 +1,7 @@
 class Api::ArtistsController < ApplicationController
   def index
-    
-    # FIXME:
-    @artists = Artist.all
     # should ony fetch the artist current user follows
-    # @artists = Artist.where('author_id = ?', params[:user_id])
-
+    @artists = Artist.where('author_id = ?', params[:user_id])
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -59,6 +59,24 @@ Playlist.create(title: Faker::Book.title, img_path: "https://picsum.photos/200/2
 
 
 
+#=====================================Artists=======================================#
+Artist.create(name: 'Adele', img_path: 'https://images-na.ssl-images-amazon.com/images/I/A15dQM39ELL._CR0,0,3840,2880_._SL1000_.jpg')
+Artist.create(name: 'Beyonce', img_path: 'https://images-na.ssl-images-amazon.com/images/I/A15dQM39ELL._CR0,0,3840,2880_._SL1000_.jpg')
+Artist.create(name: 'Bruno Mars', img_path: 'https://www.billboard.com/files/media/Bruno-Mars-press-photo-Credit-Kai-Z-Feng-2016-012a-billboard-1548.jpg')
+Artist.create(name: 'Drake', img_path: 'https://i.scdn.co/image/cb080366dc8af1fe4dc90c4b9959794794884c66')
+Artist.create(name: 'Ed Sheeran', img_path: 'https://www.billboard.com/files/media/ed-sheeran-bb33-shoot-2015-billboard-01-650.jpg')
+Artist.create(name: 'Johnny Cash', img_path: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/JohnnyCash1969.jpg')
+Artist.create(name: 'Porter Robinson', img_path: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Porter_Robinson_2014_profile_pic.png')
+Artist.create(name: 'Rihanna', img_path: 'https://static01.nyt.com/images/2015/10/25/t-magazine/25tmag-11well_rihanna-t_CA0/25tmag-11well_rihanna-t_CA0-articleLarge-v2.jpg')
+Artist.create(name: 'ZAYN', img_path: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Zayn_Wiki.jpg')
+Artist.create(name: 'The Weeknd', img_path: 'https://media.gq.com/photos/586fbf1ab730b94511591fad/master/w_1600/the-weeknd-0217-GQ-FEWE04-01-3x2.jpg')
+# Artist.create(name: '', img_path: '')
+
+
+
+
+
+
 #=====================================================================================#
 
 
