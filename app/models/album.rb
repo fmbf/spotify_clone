b2f47e137b3d06d310920bdb949b1beda7e150fa
fmbf <circumnavigate.the.globe@gmail.com>
2018-02-04 18:05:04 -0800

    run Album migration and write model validations and assocs

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,11 @@
 class Album < ApplicationRecord
+  validates :title, :artist_id, presence: true
+  after_initialize :ensure_img
+
+  belongs_to :artist
+
+  private
+  def ensure_img
+    self.img_path ||= "https://picsum.photos/200/200/?image=#{rand(100)}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204181116) do
+ActiveRecord::Schema.define(version: 20180205015847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "artist_id", null: false
+    t.string "img_path"
+    t.string "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+    t.index ["title", "artist_id"], name: "index_albums_on_title_and_artist_id", unique: true
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name", null: false
