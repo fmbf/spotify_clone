02a47edb354ead315a6465be3a7e158fc0e391c9
fmbf <circumnavigate.the.globe@gmail.com>
2018-02-04 19:31:34 -0800

    create and run Song table migration

--- db/migrate/20180205032725_create_songs.rb
+++ db/migrate/20180205032725_create_songs.rb
@@ -1,13 +1,16 @@
 class CreateSongs < ActiveRecord::Migration[5.1]
   def change
     create_table :songs do |t|
-      t.string :title
-      t.integer :artist_id
-      t.integer :album_id
-      t.string :song_path
+      t.string :title, null: false
+      t.integer :artist_id, null: false
+      t.integer :album_id, null: false
+      t.string :song_path, null: false
       t.string :duration
 
       t.timestamps
     end
+    add_index :songs, [:title, :artist_id], unique: true
+    add_index :songs, :artist_id
+    add_index :songs, :album_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205015847) do
+ActiveRecord::Schema.define(version: 20180205032725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,19 @@ ActiveRecord::Schema.define(version: 20180205015847) do
     t.string "description"
     t.index ["author_id"], name: "index_playlists_on_author_id"
     t.index ["title", "author_id"], name: "index_playlists_on_title_and_author_id", unique: true
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "artist_id", null: false
+    t.integer "album_id", null: false
+    t.string "song_path", null: false
+    t.string "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+    t.index ["title", "artist_id"], name: "index_songs_on_title_and_artist_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
